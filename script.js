36ef8ecc88665df9200b81ca55054e481081ea56
Leonardo Fogaça <leonardofogaca67@gmail.com>
2022-01-05 16:23:11 -0400

    iniciando o script para pegar os valores nos botões

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-function catchNum() {}
+let $display = document.querySelector('#display');
+
+function btntypeV() {
+  let btn = document.getElementsByClassName('btn-item');
+  let val = null;
+  for (let i = 0; i < btn.length; i++) {
+    switch (btn[i]) {
+      case btn[0]:
+        val = clear();
+        break;
+
+      default:
+        break;
+    }
+  }
+  return val;
+}
+
+function clear() {
+  $display.value = '';
+}
+
+document.addEventListener('click', btntypeV);
